f10fc81c905ef261c5a2913d755b3ed848f5fb0a
Florencia Puppo <flopy_1991@hotmail.com>
2018-11-06 01:33:11 -0300

    More test cases defined, not all of them are working. Important: review setTTL function and parameters.

--- memoization.tsx
+++ memoization.tsx
@@ -1,13 +1,22 @@
 const cache = [];
 
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
+
 function memoize(someFunction, timeout, resolver) { 
     // Pending: access someFunction first parameter to set a default value for key.
     let key = 'c544d3ae-a72d-4755-8ce5-d25db415b776';
-
-    (resolver !== undefined) ? (key = resolver (key)) : key;
+    (resolver !== undefined) ? (key = resolver(key)) : key;
 
     return (key, args) => {
         if (key in cache) {
+            console.log('Key: ' + key);
             return cache[key];
         }
         else {
@@ -20,12 +29,13 @@ function memoize(someFunction, timeout, resolver) {
 }
 
 // After timeout, if accessing cache[key] result will be undefined.
+// TODO: My cache is not the same cache that is used by memoize. Changed values never return.
 function setTTL(cache, key, timeout) {
     let timer;
-    clearTimeout(timer);
     timer = setTimeout ( function () {
+        sleep (timeout);
         delete cache[key];
-    }, timeout);
+    });
 }
 
 module.exports = {
--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 const memoization = require('./memoization.tsx');
 const expect = require('chai').expect;
 
-// hint: use https://sinonjs.org/releases/v6.1.5/fake-timers/ for faking timeouts
-
 
 let returnValue = 5;
 const testFunction =  (key) => returnValue;
+
 // Remove function after every test is done.
 function sleep(milliseconds) {
     var start = new Date().getTime();
@@ -16,26 +15,42 @@ function sleep(milliseconds) {
     }
   }
 
+function resolver (key) {
+    return 1;
+}
+
 describe('memoization', function () {
-    it('should memoize function result', () =>{
+    // Tests if value remains in cache.
+    it('should memoize function result', () =>{ 
         
-        const memoized = memoization.memoize(testFunction, 1000, (key) => key);
+        const memoized = memoization.memoize(testFunction, 1000);
         expect(memoized('c544d3ae-a72d-4755-8ce5-d25db415b776')).to.equal(5);
 
         returnValue = 10;
 
-        // Currently working. Value is saved in cache.
         expect(memoized('c544d3ae-a72d-4755-8ce5-d25db415b776')).to.equal(5);
-        console.log('Memoized es: ' + memoized('c544d3ae-a72d-4755-8ce5-d25db415b776'));
-        console.log('Return Value es: ' + returnValue);
+    });
+    // Tests if value in cache is accorded to the last change, and if it was properly deleted from cache after timeout
+    it('After 2 seconds, should memoize function result ', () =>{ 
+        
+        const memoized = memoization.memoize(testFunction, 2000);
+        expect(memoized('c544d3ae-a72d-4755-8ce5-d25db415b776')).to.equal(10);
+
+        returnValue = 15;
+        sleep (2001);
+
+        expect(memoized('c544d3ae-a72d-4755-8ce5-d25db415b776')).to.equal(15);
     });
 
-    it('After 6 seconds, should memoize function result', () =>{
+    // Tests if values are saved in cache under resolver's key.
+    it('should memoize with resolver function result', () =>{
         
-        //TODO Currently not working. Value 5 must have been removed from cache, but memoized still returns 5.
-        sleep (6000);
-        const memoized = memoization.memoize(testFunction, 5000, (key) => key);
-        expect(memoized('c544d3ae-a72d-4755-8ce5-d25db415b776')).to.equal(10);
+        const memoized = memoization.memoize(testFunction, 10000, resolver);
+        expect(memoized('c544d3ae-a72d-4755-8ce5-d25db415b776')).to.equal(15);
+
+        returnValue = 7;
+
+        expect(memoized(1)).to.equal(15);
     });
 
     // TODO additional tests required
